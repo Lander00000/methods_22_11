4027b336a1027c48088facacffaff6b7c48a62f5
Guillermo Hoffmann <84525324+glhoffmann@users.noreply.github.com>
2022-11-10 17:27:57 -0500

    added User()

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+class User():
+    def __init__(username, password, name, shippinginfo, paymentinfo):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.shippinginfo = shippinginfo
+        self.paymentinfo = paymentinfo
+        self.employee = False
+    def updateShipping(self, s):
+        self.shippinginfo = s
+    def updatePayment(self, p):
+        self.paymentinfo = p
 
