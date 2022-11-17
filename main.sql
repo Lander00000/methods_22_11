750480e1e6e4707b0d0db9ba5875afb884ebb5bb
Guillermo Hoffmann <84525324+glhoffmann@users.noreply.github.com>
2022-11-17 16:16:10 -0500

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,1 +1,40 @@
+CREATE TABLE IF NOT EXISTS User (
+  `username` VARCHAR(30) NOT NULL,
+  `password` VARCHAR(30) NOT NULL,
+  `name` VARCHAR(50) NOT NULL,
+  `shippinginfo` VARCHAR(50) NOT NULL,
+  `paymentinfo` VARCHAR(50) NOT NULL,
+  PRIMARY KEY(username))
+
+CREATE TABLE IF NOT EXISTS Cart (
+  `username` VARCHAR(30) NOT NULL,
+  `b1` INT NOt NULL,
+  `b2` INT NOt NULL,
+  `b3` INT NOt NULL,
+  `b4` INT NOt NULL,
+  `b5` INT NOt NULL,
+  `b6` INT NOt NULL,
+  `b7` INT NOt NULL,
+  `b8` INT NOt NULL,
+  `b9` INT NOt NULL,
+  `b10` INT NOt NULL,
+  PRIMARY KEY(username))
+
+CREATE TABLE IF NOT EXISTS Book (
+  `bookid` VARCHAR(5) NOT NULL,
+  `isbn` VARCHAR(20) NOT NULL,
+  `title` VARCHAR(50) NOT NULL,
+  `author` VARCHAR(50) NOT NULL,
+  `published` VARCHAR(30) NOT NULL,
+  `stock` INT NOT NULL,
+  `price` INT NOT NULL,
+  PRIMARY KEY(bookid))
+
+CREATE TABLE IF NOT EXISTS Transaction (
+  `transactionid` INT NOT NULL,
+  `username` VARCHAR(30) NOT NULL,
+  `purchaseList` VARCHAR(300) NOT NULL,
+  `cost` INT NOT NULL,
+  `date` VARCHAR(30) NOT NULL,
+)
 
