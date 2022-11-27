626d47d02b63a9c81c4b308ea2ce22194cb76a41
jgharris1 <98436606+jgharris1@users.noreply.github.com>
2022-11-27 15:31:41 -0600

    update sql
    adds auto increment, removes premade cart id's, adds genre slot

--- main.sql
+++ main.sql
@@ -1,40 +1,126 @@
-CREATE TABLE IF NOT EXISTS `User` (
-  `username` VARCHAR(30) NOT NULL,
-  `password` VARCHAR(30) NOT NULL,
-  `name` VARCHAR(50) NOT NULL,
-  `shippinginfo` VARCHAR(50) NOT NULL,
-  `paymentinfo` VARCHAR(50) NOT NULL,
-  PRIMARY KEY(`username`))
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 27, 2022 at 10:17 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
-CREATE TABLE IF NOT EXISTS `Cart` (
-  `username` VARCHAR(30) NOT NULL,
-  `b1` INT NOT NULL,
-  `b2` INT NOT NULL,
-  `b3` INT NOT NULL,
-  `b4` INT NOT NULL,
-  `b5` INT NOT NULL,
-  `b6` INT NOT NULL,
-  `b7` INT NOT NULL,
-  `b8` INT NOT NULL,
-  `b9` INT NOT NULL,
-  `b10` INT NOT NULL,
-  PRIMARY KEY(`username`))
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE IF NOT EXISTS `Book` (
-  `bookid` VARCHAR(5) NOT NULL,
-  `isbn` VARCHAR(20) NOT NULL,
-  `title` VARCHAR(50) NOT NULL,
-  `author` VARCHAR(50) NOT NULL,
-  `published` VARCHAR(30) NOT NULL,
-  `stock` INT NOT NULL,
-  `price` INT NOT NULL,
-  PRIMARY KEY(`bookid`))
 
-CREATE TABLE IF NOT EXISTS `Transaction` (
-  `transactionid` INT NOT NULL,
-  `username` VARCHAR(30) NOT NULL,
-  `purchaseList` VARCHAR(300) NOT NULL,
-  `cost` INT NOT NULL,
-  `date` VARCHAR(30) NOT NULL,
-  PRIMARY KEY(`transactionid`)
-)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `methods`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `bookid` int(11) NOT NULL,
+  `isbn` varchar(20) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `author` varchar(50) NOT NULL,
+  `published` varchar(30) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `genre` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `username` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `transactionid` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `purchaseList` varchar(300) NOT NULL,
+  `cost` int(11) NOT NULL,
+  `date` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `shippinginfo` varchar(50) NOT NULL,
+  `paymentinfo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`bookid`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`transactionid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `bookid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `transactionid` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
