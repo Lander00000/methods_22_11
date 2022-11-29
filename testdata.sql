3e6d3d942adfb8591a9bf4e3dfe929bfb01d22ba
Guillermo Hoffmann <84525324+glhoffmann@users.noreply.github.com>
2022-11-28 18:49:33 -0600

    Update testdata.sql

--- testdata.sql
+++ testdata.sql
@@ -6,13 +6,13 @@ INSERT INTO user VALUES
 ("leomessireal","g00000000l","Messi","202 Futbol St., Seattle WA 20135", "8592749254719577 3/25 832");
 
 INSERT INTO book VALUES
-("b1", "9388369696", "The Art of War", "Sun Tzu", "475BC", 20, 16, "history"),
-("b2", "0679734503", "Crime and Punishment", "Fyodor Dostoyevsky", "1867", 10, 28, "literary fiction"),
-("b3", "0593158571", "Minecraft Redstone Handbook", "Mojang", "2022", 1, 10, "tutorial"),
-("b4", "1472288157", "FORTNITE Official - The Essential Guide", "Epic Games", "2022", 10, 20, "tutorial"),
-("b5", "8411981650", "The Communist Manifesto", "Karl Marx", "1848", 10, 3, "political theory"),
-("b6", "1338741160", "Five Nights at Freddy's: The Fourth Closet", "Scott Cawthon", "2021", 13, 20, "fiction"),
-("b7", "1613835051", "Jurassic Park", "Michael Crichton", "1990", 10, 10, "science fiction"),
-("b8", "0692528318", "The Federalist Papers", "Alexander Hamilton", "1788", 10, 20, "political theory"),
-("b9", "0399501487", "Lord of the Flies", "William Golding", "1954", 10, 13, "political theory"),
-("b10", "1580898843", "Baby Loves Coding!", "Ruth Spiro", "2018", 10, 12, "tutorial");
+("1", "9388369696", "The Art of War", "Sun Tzu", "475BC", 20, 16, "history"),
+("2", "0679734503", "Crime and Punishment", "Fyodor Dostoyevsky", "1867", 10, 28, "literary fiction"),
+("3", "0593158571", "Minecraft Redstone Handbook", "Mojang", "2022", 1, 10, "tutorial"),
+("4", "1472288157", "FORTNITE Official - The Essential Guide", "Epic Games", "2022", 10, 20, "tutorial"),
+("5", "8411981650", "The Communist Manifesto", "Karl Marx", "1848", 10, 3, "political theory"),
+("6", "1338741160", "Five Nights at Freddy's: The Fourth Closet", "Scott Cawthon", "2021", 13, 20, "fiction"),
+("7", "1613835051", "Jurassic Park", "Michael Crichton", "1990", 10, 10, "science fiction"),
+("8", "0692528318", "The Federalist Papers", "Alexander Hamilton", "1788", 10, 20, "political theory"),
+("9", "0399501487", "Lord of the Flies", "William Golding", "1954", 10, 13, "political theory"),
+("10", "1580898843", "Baby Loves Coding!", "Ruth Spiro", "2018", 10, 12, "tutorial");
