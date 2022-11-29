5ef58c6171c5965abcf42cfce2886f1c1a97fc41
Guillermo Hoffmann <84525324+glhoffmann@users.noreply.github.com>
2022-11-28 18:44:31 -0600

    Create testdata.sql

---
+++ testdata.sql
@@ -0,0 +1,18 @@
+INSERT INTO user VALUES
+("jack101","doghater","Jack","333 State St., Washington DC 30942", "71580898843392039 9/23 331"),
+("these222", "nt5gtm3", "Robert", "101 Ground Blvd., Denver CO 53192", "4929553223512453 1/24 515"),
+("barryobama","m1ch3ll3","Barack","89 Portside Cir., St. Louis MI 10221", "2931738930448451 3/26 843"),
+("will3312","p455w0rd","Will","102 Grove St., Los Angeles CA, 90009", "4355203324685542 6/28 750"),
+("leomessireal","g00000000l","Messi","202 Futbol St., Seattle WA 20135", "8592749254719577 3/25 832");
+
+INSERT INTO book VALUES
+("b1", "9388369696", "The Art of War", "Sun Tzu", "475BC", 20, 16, "history"),
+("b2", "0679734503", "Crime and Punishment", "Fyodor Dostoyevsky", "1867", 10, 28, "literary fiction"),
+("b3", "0593158571", "Minecraft Redstone Handbook", "Mojang", "2022", 1, 10, "tutorial"),
+("b4", "1472288157", "FORTNITE Official - The Essential Guide", "Epic Games", "2022", 10, 20, "tutorial"),
+("b5", "8411981650", "The Communist Manifesto", "Karl Marx", "1848", 10, 3, "political theory"),
+("b6", "1338741160", "Five Nights at Freddy's: The Fourth Closet", "Scott Cawthon", "2021", 13, 20, "fiction"),
+("b7", "1613835051", "Jurassic Park", "Michael Crichton", "1990", 10, 10, "science fiction"),
+("b8", "0692528318", "The Federalist Papers", "Alexander Hamilton", "1788", 10, 20, "political theory"),
+("b9", "0399501487", "Lord of the Flies", "William Golding", "1954", 10, 13, "political theory"),
+("b10", "1580898843", "Baby Loves Coding!", "Ruth Spiro", "2018", 10, 12, "tutorial");
